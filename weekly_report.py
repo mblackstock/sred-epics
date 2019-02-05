2917ad0a5521a999ae23ff16ab30cf84656081eb
Mike Blackstock <mike.blackstock@tasktop.com>
2019-02-05 11:07:19 -0800

    Use Monday as first day of the week

--- weekly_report.py
+++ weekly_report.py
@@ -11,28 +11,18 @@ import math
 
 import config
 
-
 def week_start(date_str):
-    """Find the first/last day of the week for the given day.
-    Assuming weeks start on Sunday and end on Saturday.
-
-    Returns a tuple of ``(start_date, end_date)``.
-
+    """Find the first day of the week for the given day.
+    Assuming weeks start on Monday and ends on Sunday.
     """
     date = parse(date_str)
     # isocalendar calculates the year, week of the year, and day of the week.
     # dow is Mon = 1, Sat = 6, Sun = 7
     year, week, dow = date.isocalendar()
 
-    # Find the first day of the week.
-    if dow == 7:
-        # Since we want to start with Sunday, let's test for that condition.
-        start_date = date
-    else:
-        # Otherwise, subtract `dow` number days to get the first day of that week
-        start_date = date - timedelta(dow)
-
+    start_date = date - timedelta(dow-1)
     return start_date
+
 
 def person_week_hours(data):
     """return table of people by weeks with hours"""
